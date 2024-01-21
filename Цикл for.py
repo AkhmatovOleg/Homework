f8819b6d94d064d2d39bf902ffdc731ec659fcf1
oleg akhmatov <olleggoter@mail.ru>
2024-01-21 23:49:57 +0500

    initial

---
+++ Цикл for.py
@@ -0,0 +1,7 @@
+cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for car in cars:
+    print('Я езжу на автомабиле марки', car)
+    cars_count += 10
+    print('сколько раз', cars_count)
+print('выход из программы')
