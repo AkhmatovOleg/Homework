a16786cab33443ea6c35a4685e01f8d671170e72
oleg akhmatov <olleggoter@mail.ru>
2024-01-29 11:02:16 +0500

    исправление

--- функции.py
+++ функции.py
@@ -1,8 +1,12 @@
 def print_params(param):
-    print('Параметр',param)
-    print('Параметр',param)
+    print('Параметр', param)
+    print('Параметр', param)
 
 
 for _ in range(4):
     print_params(2)
 
+my_list = [1, 3, 4, 5]
+
+for element in my_list:
+    print_params(param=element)
