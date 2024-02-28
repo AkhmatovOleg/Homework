0bb6c5da31a579f0670a0b6b92a81b35815ccafe
oleg akhmatov <olleggoter@mail.ru>
2024-02-28 19:16:53 +0300

    initial

---
+++ Наследование классов.py
@@ -0,0 +1,34 @@
+class Car:
+    price = 1000000
+
+    def __init__(self):
+        self.horse_powers = None
+
+    def horse_powers(self):
+        self.horse_powers = 1000
+
+    def __str__(self):
+        return '{} стоимость {} количество оборотов {}'.format(
+            self.__class__.__name__, self.price, self.horse_powers)
+
+
+class Nissan(Car):
+    price = 5000000
+
+    def horse_powers(self):
+        self.horse_powers = 2000
+
+
+class Kia(Car):
+    price = 2000000
+
+    def horse_powers(self):
+        self.horse_powers = 1500
+
+
+nissan1 = Nissan()
+nissan1.horse_powers()
+print(nissan1)
+kia1 = Kia()
+kia1.horse_powers()
+print(kia1)
