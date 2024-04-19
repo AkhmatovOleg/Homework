32ad7a68ae0708aeca0f7f81ac6bdc266c049eaa
oleg akhmatov <olleggoter@mail.ru>
2024-04-19 12:50:52 +0300

    initial

--- Работа с классом Warning.py
+++ Работа с классом Warning.py
@@ -1,14 +1,24 @@
 import warnings
+
+
 def division(a, b):
     while b > 0:
-        b = b - 0.01
-        print(a/b)
-    if b == 0:
-        warnings.warn('На ноль делить нельзя',category=UserWarning)
-    print(f'b равно:{b}')
+        b -= 0.01
+        print(a / b)
+        print(f'b равно:{b}')
+        warnings.warn('На ноль делить нельзя', category=UserWarning)
 
 
+a = 1
+b = 1
+# print("Фильтр 'error'")
+# warnings.simplefilter("error", UserWarning)
+# division(a, b)
 
-a = 3
-b = 3
-print(division(a, b))
+# print("Фильтр 'ignore'")
+# warnings.simplefilter("ignore", UserWarning)
+# division(a, b)
+
+print("Фильтр 'always'")
+warnings.simplefilter("always",UserWarning)
+division(a, b)
