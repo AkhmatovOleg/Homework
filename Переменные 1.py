341dee28664a002a074abf58ea2c78a4e08702c8
oleg akhmatov <olleggoter@mail.ru>
2024-08-20 12:15:17 +0300

    initial

---
+++ Переменные 1.py
@@ -0,0 +1,6 @@
+num_of_homework = 12
+num_of_hours = 1.5
+name_course = 'Python'
+time_one_homework = num_of_hours / num_of_homework
+print('Курс:', name_course, ', всего задач:', num_of_homework, ', затрачено часов:',
+      num_of_hours, ', среднее время выполнения', time_one_homework, 'часа')
