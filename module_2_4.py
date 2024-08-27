bdf7d698781b0c616b4232e59d26bc5157eafb02
oleg akhmatov <olleggoter@mail.ru>
2024-08-27 17:48:04 +0300

    initial

--- module_2_4.py
+++ module_2_4.py
@@ -5,11 +5,12 @@ is_prime = True
 for i in numbers:
     k = 0
     for j in range(1, i + 1):
-        if i % j == 0 and i != j and j != 1:
+        if i % j == 0:
             k += 1
-    if k == 0:
+    if k == 2:
         is_prime = True
         primes.append(i)
+
     else:
         is_prime = False
         not_primes.append(i)
