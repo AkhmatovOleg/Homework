1d98369180506fe16de8c4030821582c47427f15
oleg akhmatov <olleggoter@mail.ru>
2024-08-27 11:26:17 +0300

    initial

---
+++ module_2_3.py
@@ -0,0 +1,24 @@
+my_list =[42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+x = 0
+y = len(my_list)
+while x < y:
+    a = my_list[x]
+    x += 1
+    if a > 0:
+        print(a)
+    elif a < 0:
+        break
+    continue
+
+
+
+
+
+
+
+
+
+
+
+
+
