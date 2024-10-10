e31489b522c21867e8b47330f07dbe3e8b070b0f
oleg akhmatov <olleggoter@mail.ru>
2024-10-10 14:38:25 +0300

    initial

---
+++ output/Practice_urban/sortfunc_2.py
@@ -0,0 +1,34 @@
+data = [5, 4, 2, 3, 1]
+
+
+def bubble_sort(ls):
+    for i in range(len(ls) - 1, 0, -1):
+        for j in range(i):
+            if ls[j] > ls[j + 1]:
+                ls[j], ls[j + 1] = ls[j + 1], ls[j]
+    return ls
+
+
+def selection_sort(ls):
+    for i in range(len(ls) - 1):
+        min_index = i
+        for j in range(i + 1, len(ls)):
+            if ls[min_index] > ls[j]:
+                min_index = j
+                ls[min_index], ls[j] = ls[j], ls[min_index]
+    return ls
+
+
+def insertion_sort(ls):
+    for i in range(1, len(ls)):
+        key = ls[i]
+        j = i - 1
+        while ls[j] > key and j >= 0:
+            ls[j + 1] = ls[j]
+            j -= 1
+        ls[j + 1] = key
+    return ls
+
+
+bubble_sort(data)
+print(data)
