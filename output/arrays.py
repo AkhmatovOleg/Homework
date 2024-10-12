af5bd442f7d9b846137c4a1296b6e60c22fc4161
oleg akhmatov <olleggoter@mail.ru>
2024-10-12 15:54:11 +0300

    initial

---
+++ output/arrays.py
@@ -0,0 +1,12 @@
+from Practice_urban.sortfunc_2 import bubble_sort, selection_sort, insertion_sort
+
+
+data_1 = [2, 1, 5, 6, 3, 9, 0, 8]
+
+
+bubble_sort(data_1)
+print(data_1)
+selection_sort(data_1)
+print(data_1)
+insertion_sort(data_1)
+print(data_1)
