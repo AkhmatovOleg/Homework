1d9dc2e7c53f2a76a7e6168b59137047e1b5572e
oleg akhmatov <olleggoter@mail.ru>
2024-10-15 23:23:03 +0300

    исправил

--- output/Homework/module_6_3.py
+++ output/Homework/module_6_3.py
@@ -1,20 +1,26 @@
 class Horse:
-    x_distance = 0
-    _sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
         self.x_distance += dx
 
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        super().__init__()
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         super().run(dx)
@@ -25,7 +31,7 @@ class Pegasus(Horse, Eagle):
         return pos_pegasus
 
     def voice(self):
-        print(super().sound)
+        print(self.sound)
 
 
 p1 = Pegasus()
