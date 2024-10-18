8c0fa0fc4f2c04308335d836b869bde309334284
oleg akhmatov <olleggoter@mail.ru>
2024-10-18 16:23:56 +0300

    initial

--- output/Homework/module_7_4.py
+++ output/Homework/module_7_4.py
@@ -4,8 +4,8 @@ team2_name = 'Волшебники данных'
 team2_num = 6
 score_1 = 40
 score_2 = 42
-team1_time = 18015.2
-team2_time = 21618.24
+team1_time = 1552.512
+team2_time = 2153.31451
 task_total = score_1 + score_2
 time_avg = (team1_time + team2_time) / task_total
 print('В команде %s участников:%s!' % (team1_name, team1_num))
