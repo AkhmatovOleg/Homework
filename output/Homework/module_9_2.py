45164c0300fb94e50994f8cdf4ab3575dc223d5a
oleg akhmatov <olleggoter@mail.ru>
2024-10-21 15:08:21 +0300

    initial

---
+++ output/Homework/module_9_2.py
@@ -0,0 +1,26 @@
+# Задача:
+
+# Даны несколько списков, состоящих из строк
+# first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+# second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+# 1.В переменную first_result запишите список созданный при помощи сборки состоящий из длин строк списка
+# first_strings, при условии, что длина строк не менее 5 символов.
+# 2.В переменную second_result запишите список созданный при помощи сборки состоящий из пар слов(кортежей)
+# одинаковой длины. Каждое слово из списка first_strings должно сравниваться с каждым из second_strings. (два цикла)
+# 3.В переменную third_result запишите словарь созданный при помощи сборки, где парой ключ-значение будет
+# строка-длина строки. Значения строк будут перебираться из объединённых вместе списков first_strings и second_strings.
+# Условие записи пары в словарь - чётная длина строки.
+
+
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(x) for x in first_strings if len(x) > 5]
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {str_1: len(str_1) for str_1 in first_strings + second_strings if not len(str_1) % 2}
+
+# Пример выполнения кода:
+
+print(first_result)
+print(second_result)
+print(third_result)
