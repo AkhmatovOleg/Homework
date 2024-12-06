c208f1dad0bdca82b83ad0122a083e084db93274
oleg akhmatov <olleggoter@mail.ru>
2024-12-06 22:40:58 +0300

    initial

---
+++ output/Homework_2/module_14/module_14_2.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+balance = 1000
+
+# for i in range(1, 11):
+#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+#                    (f"User{i}", f"example{i}@gmail.com", str(10 * i), balance))
+
+# cursor.execute("UPDATE Users SET balance = ? WHERE id % 2 == 1 ", (500, ))
+
+# cursor.execute("DELETE FROM Users WHERE id % 3 ==1")
+
+# cursor.execute("SELECT  username, email, age, balance FROM Users WHERE age != 60")
+# users = cursor.fetchall()
+
+
+# for username, email, age, balance in users:
+#     print(f"Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance}")
+
+# cursor.execute("DELETE FROM Users WHERE id = 6")
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+print(total_users)
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balance = cursor.fetchone()[0]
+print(all_balance)
+print(all_balance/total_users)
+connection.commit()
+connection.close()
